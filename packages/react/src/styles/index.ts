6080d97e50d304002021f6ce6839224067b7edb8
Nathan Ferreira <nathan.ferreira@pca.com.br>
2023-05-22 10:25:16 -0300

    feat: Add core components

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,38 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    // para usar os tokens de space também nas propriedades de heigh e width
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
